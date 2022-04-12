ebb696148df01e6f92ae5ae160a79518fdbc08b6
Jerry Augusto <jerryaugustodev@gmail.com>
2022-04-12 16:12:54 -0400

    Finishing main module.

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,30 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
+
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&trim_convert_evaluate/1)
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Ooops! Error while reading the file! '#{reason}'."}
+
+  defp trim_convert_evaluate(elem) do
+    elem
+    |> String.trim()
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  defp evaluate_numbers(elem) when rem(elem, 3) == 0 and rem(elem, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(elem) when rem(elem, 3) == 0, do: :fizz
+  defp evaluate_numbers(elem) when rem(elem, 5) == 0, do: :buzz
+  defp evaluate_numbers(elem), do: elem
 end
